911ea3c4a8a26c90e3e9583dbe869635b40107e5
Игорь <211353@mail.ru>
2023-08-27 01:31:30 +0300

    trouble1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import java.sql.Time;
-import java.util.Arrays;
-import java.util.Date;
+
 import java.util.Random;
-import java.util.Timer;
-
-import static java.util.Arrays.*;
-
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
@@ -40,67 +34,3 @@ public class Main {
     }
 
 }
-
-//    public static String printTextPerRole(String[] roles, String[] textLines) {
-//        StringBuilder sb = new StringBuilder();
-//        for (int i = 0; i < roles.length; i++) {
-//            sb.append(roles[i] + ":");
-//            for (int j = 0; j < textLines.length; j++) {
-//                if (textLines[j].subSequence(0,textLines[j].indexOf(':')).equals((roles[i]))) {
-//                    sb.append("\n" + (j + 1) + ")" + textLines[j].substring(textLines[j].indexOf(':') + 1));
-//                }
-//            }
-//            sb.append("\n\n");
-//        }
-//        //Твой код здесь
-//        return String.valueOf(sb);
-//    }
-//    public static String printTextPerRole1(String[] roles, String[] textLines) {
-//
-//        StringBuilder sb = new StringBuilder();
-//        String a1 = new String("foo");
-//        String a2 = new String("bar");
-//        System.out.println(a2.equals(a1) +" " + a2.hashCode());
-//        for (int i = 0; i < roles.length; i++) {
-//            sb.append(roles[i] + ":");
-//            for (int j = 0; j < textLines.length; j++) {
-//                if (textLines[j].subSequence(0,textLines[j].indexOf(':')).equals((roles[i]))) {
-////                    sb.append("\n" + (j + 1) + ")" + textLines[j].substring(textLines[j].indexOf(':') + 1));
-//                    sb.append("\n");
-//                    sb.append(j+1);
-//                    sb.append(")");
-//                    sb.append(textLines[j].substring(textLines[j].indexOf(':') + 1));
-////                    sb.append("");
-////                    sb.append("");
-////                    sb.append("");
-////                    sb.append("");
-//                }
-//            }
-//            sb.append("\n\n");
-//        }
-//        //Твой код здесь
-//        return String.valueOf(sb);
-//    }
-//
-//}
-//    public boolean equals(Object anObject) {
-//        if (this == anObject) {
-//            return true;
-//        }
-//        return (anObject instanceof String aString)
-//                && (!COMPACT_STRINGS || this.coder == aString.coder)
-//                && StringLatin1.equals(value, aString.value);
-//    }
-//public int hashCode() {
-//    int h = hash;
-//    if (h == 0 && !hashIsZero) {
-//        h = isLatin1() ? StringLatin1.hashCode(value)
-//                : StringUTF16.hashCode(value);
-//        if (h == 0) {
-//            hashIsZero = true;
-//        } else {
-//            hash = h;
-//        }
-//    }
-//    return h;
-//}
